77932f7352dd8c90d6a7d3091b2fcb8edb933031
sabeen12345 <59909957+sabeen12345@users.noreply.github.com>
2020-02-22 02:26:26 +0500

    clink and buy

--- wp-content/themes/divi-child/functions.php
+++ wp-content/themes/divi-child/functions.php
@@ -5,6 +5,8 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/style.css' );
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'load-font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+    //<i class="fas fa-cart-arrow-down"></i>
  
 }
 
